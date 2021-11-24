e7165e8592688df023723a687b8a766ebbfde962
Deepesh9511 <94192955+Deepesh9511@users.noreply.github.com>
2021-11-24 11:48:21 +0530

    Update settings.py

--- employee/settings.py
+++ employee/settings.py
@@ -118,7 +118,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
